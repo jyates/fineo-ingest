53d64f0e265b94844f1b63803bcb9ddcc2a067b6
Jesse Yates <jesse.k.yates@gmail.com>
2016-04-03 14:20:25 -0700

    using jersey for resource handling

--- beans/beans-tomcat/src/main/java/io/fineo/etl/ingest/HandleEventServlet.java
+++ beans/beans-tomcat/src/main/java/io/fineo/etl/ingest/HandleEventServlet.java
@@ -1,28 +1,19 @@
 package io.fineo.etl.ingest;
 
-import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 
 @Singleton
+@Path("/handle")
 public class HandleEventServlet extends HttpServlet {
-    private static final long serialVersionUID = 7528373021106530918L;
+  private static final long serialVersionUID = 7528373021106530918L;
 
-    private String greeting;
-
-    @Inject
-    public void setGreeting(String greeting) {
-        this.greeting = greeting;
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        resp.getOutputStream().print(greeting);
-    }
+  @POST
+  @Consumes("application/json")
+  public void handle(String body) {
+  }
 }
