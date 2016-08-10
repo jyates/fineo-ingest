d5595c59e9d3cbece4544b3b311f7f625fee41df
Jesse Yates <jesse.k.yates@gmail.com>
2016-08-09 19:17:47 -0700

    Moving deploy to top-level project

--- deploy/build-jar/lib/build/properties/properties.rb
+++
@@ -1,5 +0,0 @@
-
-require_relative '../arg_opt'
-
-module Properties
-end
--- deploy/setup-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-gem install bundler
-bundle --gemfile $DIR/Gemfile
