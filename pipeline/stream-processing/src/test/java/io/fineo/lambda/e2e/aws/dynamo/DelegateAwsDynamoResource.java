1d8093370d35777fe10784bc611b9e6d5f9d8390
Jesse Yates <jesse.k.yates@gmail.com>
2016-09-12 12:27:38 -0700

    Fixing dependencies from previous 'fix'

--- pipeline/stream-processing/src/test/java/io/fineo/lambda/e2e/aws/dynamo/DelegateAwsDynamoResource.java
+++ pipeline/stream-processing/src/test/java/io/fineo/lambda/e2e/aws/dynamo/DelegateAwsDynamoResource.java
@@ -4,28 +4,21 @@ import com.google.inject.Injector;
 import io.fineo.etl.FineoProperties;
 import io.fineo.lambda.configure.dynamo.DynamoModule;
 import io.fineo.lambda.configure.dynamo.DynamoRegionConfigurator;
-import io.fineo.lambda.dynamo.DynamoTestConfiguratorModule;
 import io.fineo.lambda.dynamo.avro.AvroToDynamoWriter;
 import io.fineo.lambda.e2e.manager.IDynamoResource;
 import io.fineo.lambda.e2e.manager.ManagerBuilder;
 import io.fineo.lambda.e2e.manager.collector.OutputCollector;
 import io.fineo.lambda.util.run.FutureWaiter;
-import io.fineo.lambda.util.run.ResultWaiter;
 import io.fineo.schema.avro.RecordMetadata;
 import io.fineo.schema.store.SchemaStore;
 import org.apache.avro.generic.GenericRecord;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static io.fineo.lambda.configure.util.InstanceToNamed.property;
-import static io.fineo.lambda.configure.util.SingleInstanceModule.instanceModule;
 import static io.fineo.lambda.e2e.validation.util.ValidationUtils.verifyRecordMatchesJson;
-import static java.lang.String.format;
-import static java.time.Duration.ofMinutes;
-import static java.time.Duration.ofSeconds;
 import static org.junit.Assert.assertEquals;
 
 /**
